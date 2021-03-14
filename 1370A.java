a8adefb371fd0e73440cb217152ea3ea2b947284
rmanishk001 <59310070+rmanishk001@users.noreply.github.com>
2021-03-14 21:28:03 +0530

    Create 1370A.java

---
+++ 1370A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class A1370 {
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    long n=sc.nextLong();
+    for (int i=0;i<n;i++)
+    {
+        long in=sc.nextLong();
+        System.out.println(in/2);
+    }
+}
+}
