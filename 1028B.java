b58186d179fdc60fd2f7624c1aa91b1f5226db50
rmanishk001 <59310070+rmanishk001@users.noreply.github.com>
2021-04-19 13:46:24 +0530

    Create 1028B.java

---
+++ 1028B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class B1028 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        for (int i=0;i<n;i++)
+        {
+            System.out.print("5");
+        }System.out.println();
+    for (int i=0;i<n-1;i++)
+    {
+    System.out.print("4");}
+    System.out.print("5");
+    }
+
+}
