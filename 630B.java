d4e741cc38e826803998b942980cb5b9ab2b900a
rmanishk001 <59310070+rmanishk001@users.noreply.github.com>
2021-04-21 21:11:49 +0530

    Create 630B.java

---
+++ 630B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B630 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        System.out.println(a*Math.pow(1.000000011,b));
+    }
+}
